9a9da6a71197f873d771ed165499ecd12ab9261e
IsBenben <162774387+IsBenben@users.noreply.github.com>
2024-11-03 10:27:06 +0800

    Update 数据排序.py

--- 数据排序.py
+++ 数据排序.py
@@ -9,9 +9,9 @@ def compile():
     lines = code.splitlines()
     nums = list(map(int, lines))
     res = sorted(nums)
-    out = map(lambda x: str(x), res)
+    out = map(lambda x: f'print({x})', res)
     with open('out_sort.py', 'w') as f:
-        f.write('import sys; sys.stdout = open("sort.txt", "w"); print("""' + '\n'.join(out) + '""")\n')
+        f.write('import sys; sys.stdout = open("sort.txt", "w");\n' + '\n'.join(out) + '\n')
     print('编译输出文件为out_sort.py')
 
 if __name__ == '__main__':
